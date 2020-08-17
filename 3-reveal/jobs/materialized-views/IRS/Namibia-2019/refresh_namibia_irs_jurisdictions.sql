185a673687c156e61b04be935a80c86f0cc68816
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-08-17 13:12:33 +0300

    Refresh all jurisdiction views at the same time
    This ensures that we can better control the order and frequency of
    refreshing these views.

--- 3-reveal/jobs/materialized-views/IRS/Namibia-2019/refresh_namibia_irs_jurisdictions.sql
+++ 3-reveal/jobs/materialized-views/IRS/Namibia-2019/refresh_namibia_irs_jurisdictions.sql
@@ -1,2 +1,6 @@
 -- Suggestion is to refresh this every 10min
+SELECT NOW();
+SELECT refresh_mat_view('namibia_focus_area_irs', TRUE);
+SELECT NOW();
 SELECT refresh_mat_view('namibia_irs_jurisdictions', TRUE);
+SELECT NOW();
