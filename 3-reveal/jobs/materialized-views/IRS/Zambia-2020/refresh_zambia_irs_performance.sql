ba3a146ead668ab35585550127f6fab241e60c91
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-10-06 11:25:57 +0300

    Add missing refresh statements for IRS performance

--- 3-reveal/jobs/materialized-views/IRS/Zambia-2020/refresh_zambia_irs_performance.sql
+++ 3-reveal/jobs/materialized-views/IRS/Zambia-2020/refresh_zambia_irs_performance.sql
@@ -1,7 +1,10 @@
 -- Suggestion is to refresh this every 1 hour
 SELECT refresh_mat_view('zambia_irs_spray_event', TRUE);
 SELECT refresh_mat_view('zambia_daily_summary_event', TRUE);
+SELECT refresh_mat_view('zambia_irs_sop_avg_time', TRUE);
 SELECT refresh_mat_view('zambia_irs_sop_performance', TRUE);
 SELECT refresh_mat_view('zambia_irs_sop_date_performance', TRUE);
+SELECT refresh_mat_view('zambia_irs_collector_avg_time', TRUE);
 SELECT refresh_mat_view('zambia_irs_data_collector_performance', TRUE);
+SELECT refresh_mat_view('zambia_irs_district_avg_time', TRUE);
 SELECT refresh_mat_view('zambia_irs_district_performance', TRUE);
