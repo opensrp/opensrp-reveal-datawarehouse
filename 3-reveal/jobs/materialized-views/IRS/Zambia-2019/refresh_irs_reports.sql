313da80e8ee13565f68d61cb2e62cf8e0152c8d3
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-10-15 19:53:45 +0300

    Add statements to refresh

--- 3-reveal/jobs/materialized-views/IRS/Zambia-2019/refresh_irs_reports.sql
+++ 3-reveal/jobs/materialized-views/IRS/Zambia-2019/refresh_irs_reports.sql
@@ -1,4 +1,5 @@
 -- Suggestion is to refresh this every 15min
-SELECT refresh_mat_view('zambia_irs_structures', TRUE);
+SELECT refresh_mat_view('zambia_irs_structures', FALSE);
+SELECT refresh_mat_view('zambia_irs_structures_data', TRUE);
 SELECT refresh_mat_view('zambia_focus_area_irs', TRUE);
 SELECT refresh_mat_view('zambia_irs_jurisdictions', TRUE);
