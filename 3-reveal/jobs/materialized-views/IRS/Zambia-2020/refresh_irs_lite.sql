6d0114b78c12509b08a6278ffbb99a6ea0cdf7ca
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-10-30 12:45:56 +0300

    Add statements to refresh IRS lite

---
+++ 3-reveal/jobs/materialized-views/IRS/Zambia-2020/refresh_irs_lite.sql
@@ -0,0 +1,4 @@
+-- Suggestion is to refresh this every 1 hour
+SELECT refresh_mat_view('zambia_irs_lite_structures', TRUE);
+SELECT refresh_mat_view('zambia_irs_lite_operational_areas', TRUE);
+SELECT refresh_mat_view('zambia_irs_lite_jurisdictions', TRUE);
