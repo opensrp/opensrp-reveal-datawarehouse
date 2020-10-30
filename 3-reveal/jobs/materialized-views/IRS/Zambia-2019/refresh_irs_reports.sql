982cc0262fb8858b809fd978f12cf4d8c0e9538e
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-10-30 15:18:44 +0300

    Use the right name of the mat view

--- 3-reveal/jobs/materialized-views/IRS/Zambia-2019/refresh_irs_reports.sql
+++ 3-reveal/jobs/materialized-views/IRS/Zambia-2019/refresh_irs_reports.sql
@@ -1,5 +1,5 @@
 -- Suggestion is to refresh this every 15min
 SELECT refresh_mat_view('zambia_irs_structures', FALSE);
-SELECT refresh_mat_view('zambia_irs_structures_data', TRUE);
+SELECT refresh_mat_view('zambia_irs_structures_report_no_geojson', TRUE);
 SELECT refresh_mat_view('zambia_focus_area_irs', TRUE);
 SELECT refresh_mat_view('zambia_irs_jurisdictions', TRUE);
