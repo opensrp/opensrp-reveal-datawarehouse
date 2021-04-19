ee20e4280381fb75b828552ec1dfeef2d351428b
ciremusyoka <wambua.eric01@gmail.com>
2021-04-19 11:38:13 +0300

    Refresh MDA Lite views

---
+++ 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
@@ -0,0 +1,7 @@
+SELECT refresh_mat_view('kenya_mda_lite_structures', TRUE);
+SELECT refresh_mat_view('kenya_mda_lite_wards_geojson', TRUE);
+SELECT refresh_mat_view('kenya_mda_lite_operational_areas', TRUE);
+SELECT refresh_mat_view('kenya_mda_lite_wards', TRUE);
+SELECT refresh_mat_view('supervisor_drug_distribution', TRUE);
+SELECT refresh_mat_view('mda_lite_plans', TRUE);
+SELECT refresh_mat_view('ward_drug_distribution', TRUE);
