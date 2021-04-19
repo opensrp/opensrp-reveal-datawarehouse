b779a8cc579542e837d3733af2faef2a835e9901
ciremusyoka <wambua.eric01@gmail.com>
2021-04-19 16:25:19 +0300

    Modify refresh statements

--- 3-reveal/jobs/materialized-views/IRS/Namibia-2019/refresh_namibia_irs_export.sql
+++ 3-reveal/jobs/materialized-views/IRS/Namibia-2019/refresh_namibia_irs_export.sql
@@ -1,2 +1,2 @@
 -- Suggestion is to refresh this every 6hr and adjust as needed
-SELECT refresh_mat_view('namibia_irs_export', TRUE);
+SELECT refresh_mat_view('namibia_irs_export', FALSE);
--- 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
+++ 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
@@ -1,5 +1,4 @@
-SELECT refresh_mat_view('kenya_mda_lite_structures', TRUE);
-SELECT refresh_mat_view('kenya_mda_lite_wards_geojson', TRUE);
+SELECT refresh_mat_view('kenya_mda_lite_structures', FALSE);
 SELECT refresh_mat_view('kenya_mda_lite_operational_areas', TRUE);
 SELECT refresh_mat_view('kenya_mda_lite_wards', TRUE);
 SELECT refresh_mat_view('supervisor_drug_distribution', TRUE);
