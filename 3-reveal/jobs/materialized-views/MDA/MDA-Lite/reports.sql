02adce2a9f0c9e1342a43b6bbaa6720f159cd5c8
ciremusyoka <wambua.eric01@gmail.com>
2021-04-30 18:16:25 +0300

    Get all words in a subcounty and add business status

--- 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
+++ 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
@@ -1,6 +1,7 @@
+
+SELECT refresh_mat_view('mda_lite_plans', TRUE);
 SELECT refresh_mat_view('kenya_mda_lite_structures', FALSE);
 SELECT refresh_mat_view('kenya_mda_lite_operational_areas', TRUE);
-SELECT refresh_mat_view('mda_lite_plans', TRUE);
 SELECT refresh_mat_view('kenya_mda_lite_jurisdictions', TRUE);
 SELECT refresh_mat_view('kenya_mda_lite_wards', TRUE);
 SELECT refresh_mat_view('supervisor_drug_distribution', TRUE);
