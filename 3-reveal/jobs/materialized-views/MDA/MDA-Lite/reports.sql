8f212aa6a866124fd2db1ce0c2c5785b134e454b
ciremusyoka <wambua.eric01@gmail.com>
2021-05-03 17:08:20 +0300

    MDA Lite aggregate populations from ward level upwards

--- 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
+++ 3-reveal/jobs/materialized-views/MDA/MDA-Lite/reports.sql
@@ -1,5 +1,6 @@
 
 SELECT refresh_mat_view('mda_lite_plans', TRUE);
+SELECT refresh_mat_view('kenya_mda_lite_wards_population', TRUE);
 SELECT refresh_mat_view('kenya_mda_lite_structures', FALSE);
 SELECT refresh_mat_view('kenya_mda_lite_operational_areas', TRUE);
 SELECT refresh_mat_view('kenya_mda_lite_jurisdictions', TRUE);
